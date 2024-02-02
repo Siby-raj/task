c5824001f43f739797ac0b35ef34b9988eec6cb4
Siby raj J <sibyraj2@gmail.com>
2024-02-02 10:20:14 +0530

    changes

--- task30/1/24/leetcode84.js
+++ task30/1/24/leetcode84.js
@@ -3,55 +3,18 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    let n=heights.length;
-    let a=[];
-    let m,k;
-    let result;
-    let right=0,left=0,left_max=0,right_max=0;
-    for(let i=0;i<n ;i++){
-        while(i<n){
-            left_max=heights[i];
-            right_max=heights[i+1];
-            if(left_max>right_max){
-                left=left_max-right_max;
-                left_max-=left;
-                result=left_max+right_max;
-                a.push(result)
-
-            }
-            else if(left_max<right_max){
-                right=right_max-left_max;
-                right_max-=right;
-                result=right_max+left_max
-                a.push(result)
-            }
-            else if(left_max===right_max){
-                result=left_max+right_max;
-                a.push(result)
-            }
-            else {
-                
-            }
-            i++;
+    let mh = 0;
+    for (let i = 0; i < heights.length; i++) {
+        let min = heights[i];
+        for (let j = i; j < heights.length; j++) {
+            min = Math.min(min, heights[j]);
+            const width = j-i+1;
+            const area = min * width;
+            mh = Math.max(mh, area);
         }
-            
-
-        } 
-        
-        
-        
-      m=Math.max(...a);
-    
-      k=Math.max(...heights);
-      if(m>n){
-          result=m
-      }
-      else{
-          result=n
-      }
-      return result
-
     }
-
-heights=[2,1,5,6,2,3]
+    console.log(mh)
+    return mh;
+};
+heights=[999,999,999,999];
 largestRectangleArea(heights)
